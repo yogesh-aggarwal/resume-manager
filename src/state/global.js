977a2791565a2906a44c00471008fcac31cdf1b1
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2020-11-29 18:04:44 +0530

    Removed garbage data

--- src/state/global.js
+++ src/state/global.js
@@ -5,15 +5,6 @@ export const resumes = new BehaviorSubject({
   "Mobile Developer": {
     name: "Mobile Developer",
   },
-  "Frontend Developer": {
-    name: "Frontend Developer",
-  },
-  "Backend Developer": {
-    name: "Backend Developer",
-  },
-  "Python Developer": {
-    name: "Python Developer",
-  },
 });
 // Stores the name of currentResume
 export const currentResume = new BehaviorSubject(Object.keys(resumes.value)[0]);
